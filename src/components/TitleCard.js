940c56122a96c25ef3d049542f352bffafbc51ac
Shan Perera <shan.per7@gmail.com>
2020-10-27 19:17:22 -0400

    Remove unused imports from TitleCard

--- src/components/TitleCard.js
+++ src/components/TitleCard.js
@@ -1,20 +1,11 @@
 import React from "react";
-import { Box, Card, CardBody, CardHeader, Text, Image } from "grommet";
+import { Box, Text, Image } from "grommet";
 import styled from "styled-components";
 import "../styles/animista.css";
 import AvatarImg from "../res/FlatCharacterAvatarWeb.svg";
 
 const StyledBackground = styled(Box)`
   background-image: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
-`;
-
-const StyledCard = styled(Card)`
-  background-image: linear-gradient(
-    to top,
-    #bdc2e8 0%,
-    #bdc2e8 1%,
-    #e6dee9 100%
-  );
 `;
 
 const StyledText = styled(Text)`
