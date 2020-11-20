fde55319049c5018c4a455101b5f0405223de26c
Shan Perera <shan.per7@gmail.com>
2020-11-20 17:05:25 -0500

    Updates

---
+++ precache-manifest.d40cc233dd1cb157d203ab285f82400e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c465a75553cb130c9edd9595ddbe86da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0bb24e913eda7c58b13",
+    "url": "/static/css/main.e34c7c1b.chunk.css"
+  },
+  {
+    "revision": "de99445b0652a20df677",
+    "url": "/static/js/2.eb2f1940.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.eb2f1940.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0bb24e913eda7c58b13",
+    "url": "/static/js/main.f69e5ba1.chunk.js"
+  },
+  {
+    "revision": "35ec47dfee4c65c275be",
+    "url": "/static/js/runtime-main.c6e37e88.js"
+  },
+  {
+    "revision": "87a8af0e1a8027f8b06739ea31472345",
+    "url": "/static/media/FlatCharacterAvatarWeb.87a8af0e.svg"
+  },
+  {
+    "revision": "e590aeb18daaa4069e7c472518e48db3",
+    "url": "/static/media/rspeer-img.e590aeb1.png"
+  },
+  {
+    "revision": "49b9475678ae846d7bade1158ac65c03",
+    "url": "/static/media/sw-image.49b94756.jpg"
+  }
+]);
